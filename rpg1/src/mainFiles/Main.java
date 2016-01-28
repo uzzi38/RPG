19f309ec7ec839b8987f593bdae3cb9494b377f7
uzzi38 <uzzi380@gmail.com>
2016-01-28 11:52:19 +0000

    Making a few small changes to make Main look nicer

--- rpg1/src/mainFiles/Main.java
+++ rpg1/src/mainFiles/Main.java
@@ -1,18 +1,8 @@
 package mainFiles;
-import archetypes.Character;
+
 public class Main {
 	public static void main(String[] args) {
-		characterCreation one = new characterCreation();
 		Party party = new Party();
-		System.out.println("Create your first character!!!");
-		party.addPartyMember(0, one.createCharacter());
-		System.out.println("Now your second");
-		party.addPartyMember(1, one.createCharacter());
-		System.out.println("Time to create your third");
-		party.addPartyMember(2, one.createCharacter());
-		System.out.println("And your final character");		
-		party.addPartyMember(3, one.createCharacter());
-		System.out.println(party.toString());
+		party.createParty();
 	}
-
 }
--- rpg1/src/mainFiles/Party.java
+++ rpg1/src/mainFiles/Party.java
@@ -1,4 +1,7 @@
 package mainFiles;
+import java.util.Arrays;
+import java.util.Scanner;
+
 import archetypes.Character;
 
 public class Party {
@@ -10,5 +13,26 @@ public class Party {
 				party[1].toString() + System.getProperty("line.separator") +
 				party[2].toString() + System.getProperty("line.separator") +
 				party[3].toString();
+	}
+	public void emptyParty() {
+		Arrays.fill(party, null);
+	}
+	public void createParty() {
+		characterCreation one = new characterCreation();
+		System.out.println("Create your first character!!!");
+		party[0] = one.createCharacter();
+		System.out.println("Now your second");
+		party[1] = one.createCharacter();
+		System.out.println("Time to create your third");
+		party[2] = one.createCharacter();
+		System.out.println("And your final character");		
+		party[3] = one.createCharacter();
+		System.out.println(toString());
+		System.out.println("Are you satisfied with this party? (Yes/No)");
+		Scanner n = new Scanner(System.in);
+		if (n.nextLine().equals("No")){
+			emptyParty();
+			createParty();
+		}
 	}
 }
